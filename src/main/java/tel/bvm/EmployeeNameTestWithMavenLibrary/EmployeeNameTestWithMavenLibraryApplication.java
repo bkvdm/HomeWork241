bd3e33581989d43602100e905050aa3fc2ec1690
bkvdm <vadim@991199.ru>
2023-11-06 18:07:17 +0300

    Домашняя работа №24 "Библиотеки". Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75302/homework-requirements

---
+++ src/main/java/tel/bvm/EmployeeNameTestWithMavenLibrary/EmployeeNameTestWithMavenLibraryApplication.java
@@ -0,0 +1,13 @@
+package tel.bvm.EmployeeNameTestWithMavenLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeNameTestWithMavenLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeNameTestWithMavenLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/EmployeeNameTestWithMavenLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package tel.bvm.EmployeeNameTestWithMavenLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeNameTestWithMavenLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
