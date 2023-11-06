e529f8b04b12c45b8b27ef5bd292e83f3f2d9ec5
bkvdm <vadim@991199.ru>
2023-11-06 20:36:24 +0300

    Домашняя работа №24 "Библиотеки". Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75302/homework-requirements

--- src/main/java/tel/bvm/EmployeeNameTestWithMavenLibrary/EmployeeNameTestWithMavenLibraryApplication.java
+++ src/main/java/tel/bvm/EmployeeNameTestWithMavenLibrary/EmployeeNameTestWithMavenLibraryApplication.java
@@ -2,12 +2,22 @@ package tel.bvm.EmployeeNameTestWithMavenLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.commons.lang3.StringUtils;
 
 @SpringBootApplication
 public class EmployeeNameTestWithMavenLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeNameTestWithMavenLibraryApplication.class, args);
-	}
+//		String nameEmployee = "Нина";
 
+//		boolean checkNameEmployee = StringUtils.isAlpha(nameEmployee) && StringUtils.isWhitespace(nameEmployee);
+
+//		if (checkNameEmployee) {
+//		if (StringUtils.isAlpha(nameEmployee)) {
+//			System.out.println("Имя и фамилие пользователя введено верно");
+//		} else {
+//			System.out.println("Имя и фамилие пользователя содержит недопустимые символы");
+//		}
+	}
 }
