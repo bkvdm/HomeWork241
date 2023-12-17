894994f8ce4ce2cbdb5f881ba93c27454ac135b7
bkvdm <vadim@991199.ru>
2023-12-17 13:11:29 +0300

    Домашняя работа №27 "Spring и Mockito". Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75302/homework-requirements

--- src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/service/DetailsEmployeeDepartmentServiceImplTest.java
+++ src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/service/DetailsEmployeeDepartmentServiceImplTest.java
@@ -1,24 +1,30 @@
 package tel.bvm.EmployeeNameTestWithMavenLibrary.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tel.bvm.EmployeeNameTestWithMavenLibrary.constants.EmployeeConstants;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 
 public class DetailsEmployeeDepartmentServiceImplTest {
 
+    @Mock
+    private EmployeeServiceImpl serviceMock;
 
-    @InjectMocks
-    private DetailsEmployeeDepartmentServiceImpl detailsEmployeeDepartmentServiceImpl;
+    @BeforeEach
+    void setUp() {
+        DetailsEmployeeDepartmentService out = new DetailsEmployeeDepartmentServiceImpl(serviceMock);
+        when(serviceMock.getMap()).thenReturn(EmployeeConstants.MAP_EMPLOYEE);
+    }
 
     @Test
-    public void maxWageDepartment() {
-//        Mockito.when()
+    public void maxWageDepartmentVerify() {
+
     }
-    //    maxWageDepartment
-//    minWageDepartment
-//    allEmployeeInfo
-//    allEmployeeGroup
 }
