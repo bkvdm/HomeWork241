bc8359bf9573daf34bc8ceb4bee5c8e7c1580dde
bkvdm <vadim@991199.ru>
2023-12-24 21:48:31 +0300

    Домашняя работа №27 "Spring и Mockito". Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75302/homework-requirements

--- src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/constants/EmployeeConstants.java
+++ src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/constants/EmployeeConstants.java
@@ -12,15 +12,12 @@ public class EmployeeConstants {
     public static final Employee FOUR_EMPLOYEE = new Employee("Владимиров", "Владимир", "02020202", 1990, 400000, 2);
     public static final Employee FIVE_EMPLOYEE = new Employee("Тимофеев", "Тимофей", "303030303", 1995, 500000, 1);
     public static final Employee SIX_EMPLOYEE = new Employee("Маруся", "Смирнова", "123", 2000, 500000, 6);
-
     public static final String ID_FIRST = FIRST_EMPLOYEE.getFirstName() + FIRST_EMPLOYEE.getLastName() + FIRST_EMPLOYEE.getPasswordNumber();
     public static final String ID_SECOND = SECOND_EMPLOYEE.getFirstName() + SECOND_EMPLOYEE.getLastName() + SECOND_EMPLOYEE.getPasswordNumber();
     public static final String ID_THREE = THREE_EMPLOYEE.getFirstName() + THREE_EMPLOYEE.getLastName() + THREE_EMPLOYEE.getPasswordNumber();
     public static final String ID_FOUR = FOUR_EMPLOYEE.getFirstName() + FOUR_EMPLOYEE.getLastName() + FOUR_EMPLOYEE.getPasswordNumber();
     public static final String ID_FIVE = FIVE_EMPLOYEE.getFirstName() + FIVE_EMPLOYEE.getLastName() + FIVE_EMPLOYEE.getPasswordNumber();
     public static final String ID_SIX = SIX_EMPLOYEE.getFirstName() + SIX_EMPLOYEE.getLastName() + SIX_EMPLOYEE.getPasswordNumber();
-
-
 
     public static final Map<String, Employee> MAP_EMPLOYEE = Map.of(
             "ИванИванов10101010", new Employee("Иван", "Иванов", "10101010", 1975, 100000, 5),
