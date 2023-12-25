2c6b65cbcf857287d86a4dd745dcc51ffc8bc635
bkvdm <vadim@991199.ru>
2023-12-25 20:29:57 +0300

    Домашняя работа №27 "Spring и Mockito". Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75302/homework-requirements

--- src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/constants/EmployeeConstants.java
+++ src/test/java/tel/bvm/EmployeeNameTestWithMavenLibrary/constants/EmployeeConstants.java
@@ -20,10 +20,11 @@ public class EmployeeConstants {
     public static final String ID_SIX = SIX_EMPLOYEE.getFirstName() + SIX_EMPLOYEE.getLastName() + SIX_EMPLOYEE.getPasswordNumber();
 
     public static final Map<String, Employee> MAP_EMPLOYEE = Map.of(
-            "ИванИванов10101010", new Employee("Иван", "Иванов", "10101010", 1975, 100000, 5),
-            "ПетровПётр01010101", new Employee("Петров", "Пётр", "01010101", 1980, 200000, 4),
-            "СмирновСемён20202020", new Employee("Смирнов", "Семён", "20202020", 1985, 300000, 3),
-            "ВладимировВладимир02020202", new Employee("Владимиров", "Владимир", "02020202", 1990, 400000, 2),
-            "ТимофеевТимофей303030303", new Employee("Тимофеев", "Тимофей", "303030303", 1995, 500000, 1)
+            ID_FIRST, new Employee(FIRST_EMPLOYEE.getFirstName(), FIRST_EMPLOYEE.getLastName(), FIRST_EMPLOYEE.getPasswordNumber(), FIRST_EMPLOYEE.getYearBirth(), FIRST_EMPLOYEE.getWage(), FIRST_EMPLOYEE.getDepartmentNumber()),
+            ID_SECOND, new Employee(SECOND_EMPLOYEE.getFirstName(), SECOND_EMPLOYEE.getLastName(), SECOND_EMPLOYEE.getPasswordNumber(), SECOND_EMPLOYEE.getYearBirth(), SECOND_EMPLOYEE.getWage(), SECOND_EMPLOYEE.getDepartmentNumber()),
+            ID_THREE, new Employee(THREE_EMPLOYEE.getFirstName(), THREE_EMPLOYEE.getLastName(), THREE_EMPLOYEE.getPasswordNumber(),THREE_EMPLOYEE.getYearBirth(),THREE_EMPLOYEE.getWage(), THREE_EMPLOYEE.getDepartmentNumber()),
+            ID_FOUR, new Employee(FOUR_EMPLOYEE.getFirstName(), FOUR_EMPLOYEE.getLastName(), FOUR_EMPLOYEE.getPasswordNumber(), FOUR_EMPLOYEE.getYearBirth(), FOUR_EMPLOYEE.getWage(), FOUR_EMPLOYEE.getDepartmentNumber()),
+            ID_FIVE, new Employee(FIVE_EMPLOYEE.getFirstName(), FIVE_EMPLOYEE.getLastName(), FIVE_EMPLOYEE.getPasswordNumber(), FIVE_EMPLOYEE.getYearBirth(), FIVE_EMPLOYEE.getWage(), FIVE_EMPLOYEE.getDepartmentNumber()),
+            ID_SIX, new Employee(SIX_EMPLOYEE.getFirstName(), SIX_EMPLOYEE.getLastName(), SIX_EMPLOYEE.getPasswordNumber(), SIX_EMPLOYEE.getYearBirth(), SIX_EMPLOYEE.getWage(), SIX_EMPLOYEE.getDepartmentNumber())
     );
 }
